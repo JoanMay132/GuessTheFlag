2edd052cd5cde1efef99929d015c6985562fd789
JoanMay132 <jcmd270@gmail.com>
2024-09-19 00:49:55 -0600

    Initial Commit

---
+++ GuesTheFlag/GuesTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesTheFlagApp.swift
+//  GuesTheFlag
+//
+//  Created by Joan May on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GuesTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
